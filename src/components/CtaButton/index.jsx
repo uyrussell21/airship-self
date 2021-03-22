9e77d3c1ec233e839dc216eabc24ec82de3a2c54
uyrussell21 <uyrussell@gmail.com>
2021-03-22 08:25:05 +0800

    edit: navdrop down Overview item full width edit: Cta Button links

--- src/components/CtaButton/index.jsx
+++ src/components/CtaButton/index.jsx
@@ -1,7 +1,8 @@
 import Link from "next/link"
 
 const CtaButton = ({ className, children }) => (
-  <Link href="/">
+
+  <Link href={children ? "/features/feature-overview" : "/"}>
     <a className={`cta-btn ${className}`}>{children || "Schedule a Demo"}</a>
   </Link>
 )
