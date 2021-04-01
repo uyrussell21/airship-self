5f34e775ad0f1559269323df3ec90aaeeafe300f
uyrussell21 <uyrussell@gmail.com>
2021-04-01 18:27:36 +0800

    edit: Cta button external link edit: Cta section img border radius

--- src/components/CtaButton/index.jsx
+++ src/components/CtaButton/index.jsx
@@ -1,9 +1,14 @@
 import Link from "next/link"
 
 const CtaButton = ({ className, children }) => (
-
-  <Link href={children ? "/features/feature-overview" : "/"}>
-    <a className={`cta-btn ${className}`}>{children || "Schedule a Demo"}</a>
+  <Link
+    href={children
+      ? "/features/feature-overview"
+      : "https://www.calendly.com/airshiplogistics"}
+  >
+    <a className={`cta-btn ${className}`} target={children ? undefined : "_blank"}>
+      {children || "Schedule a Demo"}
+    </a>
   </Link>
 )
 
