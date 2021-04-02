e685ae618635870c48ba9113ea36d736b7196b73
uyrussell21 <uyrussell@gmail.com>
2021-04-02 15:02:06 +0800

    edit: added target and rel for CTA button

--- src/components/CtaButton/index.jsx
+++ src/components/CtaButton/index.jsx
@@ -1,15 +1,22 @@
 import Link from "next/link"
 
-const CtaButton = ({ className, children }) => (
-  <Link
-    href={children
-      ? "/features/feature-overview"
-      : "https://www.calendly.com/airshiplogistics"}
-  >
-    <a className={`cta-btn ${className}`} target={children ? undefined : "_blank"}>
-      {children || "Schedule a Demo"}
-    </a>
-  </Link>
-)
+const CtaButton = ({ className, children }) => {
+  let target = "_blank"
+  let rel = "_blank"
+  let href = "https://www.calendly.com/airshiplogistics"
+  if (children) {
+    target = undefined
+    rel = undefined
+    href = "/features/feature-overview"
+  }
+
+  return (
+    <Link href={href} >
+      <a className={`cta-btn ${className}`} {...{target, rel}}>
+        {children || "Schedule a Demo"}
+      </a>
+    </Link>
+  )
+}
 
 export default CtaButton
