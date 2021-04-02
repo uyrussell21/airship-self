85eb728a3bc463602efcf8231b27dae91cad5304
uyrussell21 <uyrussell@gmail.com>
2021-04-02 20:46:05 +0800

    add: About page styles edit: Footer padding

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -22,7 +22,7 @@ const valuesContent = [
   {
     head: "Open Communication",
     lead: "Transparency and open communication is highly valued in the workplace. We believe feedback is best given real-time."
-  },
+  }
 ]
 
 const HeroSection = () => (
@@ -34,49 +34,68 @@ const HeroSection = () => (
 )
 
 const OurStory = () => (
-  <section>
-    <h2>Our Story</h2>
-    <h3>Started as BotBros</h3>
-    <p>Around 2012, Rachelle and Charlotte started selling things online and has been utilizing several local courier companies to deliver the orders. Every year, they had to change the local courier because of either cash or management problems. Rachelle wanted to fix the logistics process so bad. Luckily around 2016, one of the local couriers had the budget and was looking for a web development agency to build the software for them.</p>
-
-    <p>BotBros AI was born. The first logistics system was created, and many other systems were built - not just in the logistics industry but various platforms. Eventually, BotBros saw the opportunity in the need for a stronger logistics infrastructure to support the booming online commerce.</p>
-
-    <iframe src="https://player.vimeo.com/video/306620068" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-
-    <h3>All-in on Airship</h3>
-    <p>Fast forward to 2019, BotBros entered Airship into the Ideaspace Startup Acceleration program and won the competition. BotBros went all in and never looked back ever since.</p>
-
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/VwfPZzZIw6c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <section className="our story">
+    <div className="container-sm">
+      <h2>Our Story</h2>
+      <div>
+        <h3>Started as BotBros</h3>
+        <p>Around 2012, Rachelle and Charlotte started selling things online and has been utilizing several local courier companies to deliver the orders. Every year, they had to change the local courier because of either cash or management problems. Rachelle wanted to fix the logistics process so bad. Luckily around 2016, one of the local couriers had the budget and was looking for a web development agency to build the software for them.</p>
+        <p>BotBros AI was born. The first logistics system was created, and many other systems were built - not just in the logistics industry but various platforms. Eventually, BotBros saw the opportunity in the need for a stronger logistics infrastructure to support the booming online commerce.</p>
+        <div className="video-box">
+          <iframe src="https://player.vimeo.com/video/306620068" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+        </div>
+        <h3>All-in on Airship</h3>
+        <p>Fast forward to 2019, BotBros entered Airship into the Ideaspace Startup Acceleration program and won the competition. BotBros went all in and never looked back ever since.</p>
+        <div className="video-box">
+          <iframe src="https://www.youtube.com/embed/VwfPZzZIw6c?rel=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+      </div>
+    </div>
   </section>
 )
 
 const OurValues = () => (
-  <section>
-    <h2>Our Values</h2>
-    <img src="/static/about/about-values.png" alt="Our Values graphic"/>
-    {valuesContent.map(({head, lead}) => (
+  <section className="our values">
+    <div className="container-sm">
       <div>
-        <h3>{head}</h3>
-        <p>{lead}</p>
+        <h2>Our Values</h2>
+        <img src="/static/about/about-values.png" alt="Our Values graphic"/>
       </div>
-    ))}
+      <div className="values-list">
+        {valuesContent.map(({head, lead}) => (
+          <div key={head}>
+            <h3>{head}</h3>
+            <p>{lead}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   </section>
 )
 
 const LifeAtAirship = () => (
-  <section>
-    <img src="/static/about/about-life.png" alt="the Airship crew"/>
-    <h2>Life at Airship</h2>
-    <p>Our growing team is composed of young and dynamic individuals. passionate about our goal to revolutionize the logistics industry. Airship embodies a culture of support and work-life balance to its people, ensuring that everyone is always highly valued.</p>
+  <section className="life">
+    <div className="container-sm">
+      <h2>Life at Airship</h2>
+      
+      <img src="/static/about/about-team.png" alt="the Airship crew"/>
+
+      <p>Our growing team is composed of young and dynamic individuals. passionate about our goal to revolutionize the logistics industry. Airship embodies a culture of support and work-life balance to its people, ensuring that everyone is always highly valued.</p>
+    </div>
   </section>
 )
 
 const About = () => (
-  <main>
+  <main id="about">
     <HeroSection />
     <OurStory />
     <OurValues />
     <LifeAtAirship />
+    <style jsx global>{`
+      body {
+        background-color: #fff;
+      }
+    `}</style>
   </main>
 )
 
